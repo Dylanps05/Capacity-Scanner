a5ab455bc334a5ad2ce17905e7448c5ec91ada41
11Spades <77303844+11Spades@users.noreply.github.com>
2025-11-17 10:49:37 -0500

    chore: Implement site glue class

---
+++ internal/site.go
@@ -0,0 +1,33 @@
+package internal
+
+import (
+	"context"
+	"github.com/Dylanps05/Capacity-Scanner/internal/logic"
+	"github.com/Dylanps05/Capacity-Scanner/internal/storage"
+	"github.com/Dylanps05/Capacity-Scanner/internal/web"
+	"github.com/jackc/pgx/v5"
+	"log"
+)
+
+type Site struct {
+	db *pgx.Conn
+	storage.SiteStorage
+	logic.Controller
+	web.Handler
+}
+
+func (s *Site) initSQL(db_addr string) {
+	conn, err := pgx.Connect(context.Background(), db_addr)
+	if err != nil {
+		log.Fatal(err)
+	}
+	s.db = conn
+}
+
+func (s *Site) Init(web_addr string, db_addr string) {
+	s.initSQL(db_addr)
+	s.SiteStorage = storage.NewDefaultSiteStorage(s.db)
+	//s.Controller = logic.DefaultController{}.New(s.SiteStorage)
+	//s.Handler = web.DefaultHandler{}.New(s.Controller)
+	s.Handler.Start(web_addr)
+}
