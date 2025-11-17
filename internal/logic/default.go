c92e6876ecfa17868c5fdd11ed9f43ec6bd2160a
11Spades <77303844+11Spades@users.noreply.github.com>
2025-11-17 11:53:19 -0500

    chore: Add sensor logic to glue

--- internal/logic/default.go
+++ internal/logic/default.go
@@ -1,11 +1,16 @@
 package logic
 
 import (
+	"github.com/Dylanps05/Capacity-Scanner/internal/logic/sensor"
 	"github.com/Dylanps05/Capacity-Scanner/internal/storage"
 )
 
-type DefaultController struct{}
+type DefaultController struct {
+	sensor.SensorModule
+}
 
 func NewDefaultController(s storage.SiteStorage) Controller {
-	return &DefaultController{}
+	return &DefaultController{
+		SensorModule: sensor.NewDefaultSensorModule(s.GetSensorAuthStorage(), s.GetCapacityStorage()),
+	}
 }
--- internal/logic/iface.go
+++ internal/logic/iface.go
@@ -1,4 +1,9 @@
 package logic
 
+import (
+	"github.com/Dylanps05/Capacity-Scanner/internal/logic/sensor"
+)
+
 type Controller interface {
+	sensor.SensorModule
 }
