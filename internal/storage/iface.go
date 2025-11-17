45e5a4044faaebaa965ccc20ccdb400ef6933583
11Spades <77303844+11Spades@users.noreply.github.com>
2025-11-17 12:35:19 -0500

    chore: Correct erroneous interface function

--- internal/storage/iface.go
+++ internal/storage/iface.go
@@ -3,11 +3,9 @@ package storage
 import (
 	"github.com/Dylanps05/Capacity-Scanner/internal/storage/capacity"
 	"github.com/Dylanps05/Capacity-Scanner/internal/storage/sensor"
-	"github.com/jackc/pgx/v5"
 )
 
 type SiteStorage interface {
-	New(db *pgx.Conn) SiteStorage
 	GetCapacityStorage() capacity.CapacityStorage
 	GetSensorAuthStorage() sensor.SensorAuthStorage
 }
